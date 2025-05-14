3794b4b9f147d614cee86db6cfe4a76baf61d23d
Emeraldstarr <134674589+Emeraldstarr@users.noreply.github.com>
2025-05-13 23:25:24 -0700

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "next-themes";
+import NotFound from "@/pages/not-found";
+import PropertyCalculator from "@/pages/PropertyCalculator";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={PropertyCalculator} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
